931e7ee45847f85c0c70cfdcf5f8081f482a2143
mihango <mihangokennedy@gmail.com>
2021-10-26 17:51:40 +0300

    git ignore target

--- random_microservice/src/main.rs
+++ random_microservice/src/main.rs
@@ -2,15 +2,26 @@ use hyper::{Body, Response, Server};
 use hyper::rt::Future;
 use hyper::service::service_fn_ok;
 
+use log::{debug, info, trace};
+
+
 fn main() {
-    let address = ([127, 0, 0, 1], 8001).into();
-    let builder = Server::bind(&address);
+    pretty_env_logger::init();
+
+    info!("Rand Microservice - v0.1.0");
+    trace!("Starting...");
+    let addr = ([127, 0, 0, 1], 8001).into();
+    debug!("Trying to bind server to address {}", addr);
+    let builder = Server::bind(&addr);
+    trace!("Creating service handler...");
     let server = builder.serve(|| {
         service_fn_ok(|_| {
             let random_byte = rand::random::<u8>();
             Response::new(Body::from(random_byte.to_string()))
         })
     });
+    info!("Used address: {}", server.local_addr());
     let server = server.map_err(drop);
-    hyper::rt::run(server)
+    debug!("Run!");
+    hyper::rt::run(server);
 }
